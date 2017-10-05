32bb17128c816e7d1c586af2f819b649ec5d7702
Pawel Zawitowski <zawito@gmail.com>
2017-10-05 20:58:07 +0200

    Test commit 2

--- TestModule/src/FirstTestClass.java
+++ TestModule/src/FirstTestClass.java
@@ -3,6 +3,6 @@
  */
 public class FirstTestClass {
     public static void main(String[] args) {
-        System.out.println("This is a test!");
+        System.out.println("This is a test after change!");
     }
 }
