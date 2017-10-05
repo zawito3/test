e97f08c69501b8dc18a4c9bd734ec7fe01f370f4
Pawel Zawitowski <zawito@gmail.com>
2017-10-05 21:00:31 +0200

    Test commit 2

--- TestModule/src/FirstTestClass.java
+++ TestModule/src/FirstTestClass.java
@@ -4,5 +4,6 @@
 public class FirstTestClass {
     public static void main(String[] args) {
         System.out.println("This is a test after change!");
+        System.out.println("This is a commit in branch test!");
     }
 }
