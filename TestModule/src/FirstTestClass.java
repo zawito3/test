069fc2fbf09d794b4c7b06d39de608fa5f61752c
Pawel Zawitowski <zawito@gmail.com>
2017-10-16 21:51:22 +0200

    Branch test comment.

--- TestModule/src/FirstTestClass.java
+++ TestModule/src/FirstTestClass.java
@@ -5,5 +5,6 @@ public class FirstTestClass {
     public static void main(String[] args) {
         System.out.println("This is a test after change!");
         System.out.println("This is a commit in branch test!");
+        System.out.println("Branch test!");
     }
 }
