8bfcb44a58c81ba82a7afbdbfaf93c606378f099
Pawel Zawitowski <zawito@gmail.com>
2018-09-23 19:47:35 +0200

    New field added.

--- TestModule/src/ThirdTestClass.java
+++ TestModule/src/ThirdTestClass.java
@@ -5,4 +5,13 @@ public class ThirdTestClass {
     private String className = "This third test class";
     /* Commit after tag Version1.0*/
     /* Commit after tag Version1.1*/
+    private String superField;
+
+    public String getSuperField() {
+        return superField;
+    }
+
+    public void setSuperField(String superField) {
+        this.superField = superField;
+    }
 }
